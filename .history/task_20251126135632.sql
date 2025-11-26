0367570e6ee6f9b4e6d50c0a6ad0c9f0ad730745
Stas Sytnykov <stassytnykov@gmail.com>
2025-11-26 18:08:52 +0200

    chore: delete history

--- .history/task_20251126135632.sql
+++
@@ -1,5 +0,0 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
-
-CREATE INDEX idx_customer ON Customers (Email); 
-CREATE INDEX idx_product ON Products (Name); 
--- .history/task_20251126180746.sql
+++
@@ -1,5 +0,0 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
-
-CREATE INDEX Email ON Customers (Email); 
-CREATE INDEX Name ON Products (Name); 
